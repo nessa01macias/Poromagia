dfc6ca7a55e81ffcb8e3c7b3492bc2fe350c89c4
Schwalb Sophia <ss513@hdm-stuttgart.de>
2022-11-18 17:34:27 +0100

    add dropdown to select diagram type and dates in statistics component

--- frontend/src/app/statistics/statistics.component.ts
+++ frontend/src/app/statistics/statistics.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+type DiagramType = {
+  id: number,
+  text: string,
+  selected: boolean
+}
+
 @Component({
   selector: 'app-statistics',
   templateUrl: './statistics.component.html',
@@ -7,9 +13,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StatisticsComponent implements OnInit {
 
+  dropdownOpen: boolean = false;
+  diagramTypes: DiagramType[] = [
+    {id: 0, text: 'Number of sorted cards', selected: false},
+    {id: 1, text: 'Number of not recognized cards', selected: false},
+    {id: 2, text: 'Selected category', selected: false}
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleSelectDropdown(): void {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  selectDiagramType(typeId: number) {
+    this.diagramTypes.forEach(type => {
+      type.selected = type.id === typeId;
+    });
   }
 
 }
