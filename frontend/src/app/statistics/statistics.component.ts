9b240730fa0bb932b133742f18bea6c01f78c639
Schwalb Sophia <ss513@hdm-stuttgart.de>
2022-11-22 12:31:42 +0100

    call http method according to selected diagram in statistics component

--- frontend/src/app/statistics/statistics.component.ts
+++ frontend/src/app/statistics/statistics.component.ts
@@ -1,13 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {HttpService} from "../_services/http.service";
-import {Observable} from "rxjs";
+import {functionName, HttpService} from "../_services/http.service";
 
 type DiagramType = {
   id: number,
   text: string,
   selected: boolean,
-  endpointMethod: ((fromDate: Date, toDate: Date) => Observable<any>)
-    | ((fromDate: Date, toDate: Date, boxId: number) => Observable<any>)
+  endpointMethod: functionName
 }
 
 @Component({
@@ -19,13 +17,12 @@ export class StatisticsComponent implements OnInit {
 
   dropdownOpen: boolean = false;
   diagramTypes: DiagramType[] = [
-    {id: 0, text: 'Number of sorted cards', selected: false, endpointMethod: this.httpService.getNumberOfAllCards},
-    {id: 1, text: 'Number of recognized cards', selected: false, endpointMethod: this.httpService.getNumberOfRecognizedCards},
-    {id: 2, text: 'Number of not recognized cards', selected: false, endpointMethod: this.httpService.getNumberOfNotRecognizedCards},
-    {id: 3, text: 'Number of cards sorted in boxes', selected: false, endpointMethod: this.httpService.getNumberOfCardsInBox},
-    {id: 4, text: 'Number of cards per category', selected: false, endpointMethod: this.httpService.getCategoriesCount}, //TODO: no graph
-    {id: 5, text: 'Start and end time for categories', selected: false, endpointMethod: this.httpService.getSortingDataCategories}, //TODO: no graph
-    //{id: 6, text: 'Average time to sort one card', selected: false},
+    {id: 0, text: 'Number of sorted cards', selected: false, endpointMethod: 'ALL_CARDS'},
+    {id: 1, text: 'Number of recognized cards', selected: false, endpointMethod: 'RECOGNIZED_CARDS'},
+    {id: 2, text: 'Number of not recognized cards', selected: false, endpointMethod: 'NOT_RECOGNIZED_CARDS'},
+    {id: 3, text: 'Number of cards sorted in boxes', selected: false, endpointMethod: 'CARDS_IN_BOXES'},
+    {id: 4, text: 'Number of cards per category', selected: false, endpointMethod: 'CATEGORIES_COUNT'}, //TODO: no graph
+    {id: 5, text: 'Start and end time for categories', selected: false, endpointMethod: 'SORTING_DATA_CATEGORIES'}, //TODO: no graph
   ];
 
   fromDate: Date | undefined = undefined;
@@ -46,24 +43,38 @@ export class StatisticsComponent implements OnInit {
 
   toggleSelectDropdown(): void {
     if (this.dropdownOpen && this.fromDate && this.toDate) {
-      this.xAxisValues = this.getDates(this.fromDate, this.toDate);
-      const datasetValues: number[] = [];
-      this.yAxisValues = [];
-      this.httpService.getNumberOfAllCards(this.fromDate, this.toDate) //TODO: use defined method for selected type
-        .subscribe(res => {
-          //TODO: get number of response data, add label and dataset for each data
-          for (let date of this.xAxisValues) {
-            const numberOfCards = res.find((data: {_id: string, count: number}) => data._id === date);
-            datasetValues.push(numberOfCards ? numberOfCards.count : 0);
-          }
-          this.yAxisValues.push(datasetValues);
-          this.graphTitle = this.diagramTypes.find(type => type.selected)!.text;
-          this.yAxisTitle = 'Number of cards'; //TODO
-          this.xAxisTitle = 'Dates';
-          this.datasetLabels = [this.diagramTypes.find(type => type.selected)!.text]; //TODO: only call find once
-          this.tension = [0];
-          this.dropdownOpen = !this.dropdownOpen;
-        });
+      const selectedType = this.diagramTypes.find(type => type.selected);
+      if (selectedType && selectedType.endpointMethod) {
+        this.xAxisValues = this.getDates(this.fromDate, this.toDate);
+        this.yAxisValues = [];
+        this.datasetLabels = [];
+        this.tension = [];
+
+        this.httpService.callStatisticsEndpoint(selectedType.endpointMethod, this.fromDate, this.toDate)
+          .subscribe(res => {
+            const responseProperties = Object.getOwnPropertyNames(res[0]);
+            const datasetValues: number[][] = [];
+            for (let i = 1; i < responseProperties.length; i++) {
+              datasetValues.push([]);
+              this.datasetLabels.push(responseProperties[i]);
+              this.tension.push(0);
+            }
+
+            for (let date of this.xAxisValues) {
+              const numberOfCards = res.find((data: {_id: string, count: number}) => data._id === date);
+              for (let i = 1; i < responseProperties.length; i++) {
+                datasetValues[i - 1].push(numberOfCards ? numberOfCards[responseProperties[i]] : 0);
+              }
+            }
+
+            this.yAxisValues = datasetValues;
+            this.graphTitle = selectedType.text;
+            this.yAxisTitle = 'Number of cards'; //TODO
+            this.xAxisTitle = 'Dates';
+
+            this.dropdownOpen = !this.dropdownOpen;
+          });
+      }
     } else {
       this.dropdownOpen = !this.dropdownOpen;
     }
