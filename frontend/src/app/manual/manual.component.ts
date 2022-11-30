ad0025bdfc1ed139cfda98d14b6d1f75c53cd321
Schwalb Sophia <ss513@hdm-stuttgart.de>
2022-11-30 10:27:32 +0100

    add dropdowns to user manual page

--- frontend/src/app/manual/manual.component.ts
+++ frontend/src/app/manual/manual.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ManualComponent implements OnInit {
 
+  // contains a boolean value for each dropdown indicating if the dropdown is open (true) or not (false)
+  dropdownOpenValues = [false, false, false];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  toggleDropdownStatus(dropdownIndex: number): void {
+    this.dropdownOpenValues[dropdownIndex] = !this.dropdownOpenValues[dropdownIndex];
   }
 
 }
