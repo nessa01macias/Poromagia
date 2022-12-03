fd7ac4e4c21c1ba0650926f0c186d612b980b293
Melany Macias <nessa01macias@gmail.com>
2022-12-04 01:39:04 +0200

    updated dist

--- backend.js
+++ backend.js
@@ -13,14 +13,14 @@ const http = require('http').createServer(app);
 
 
 // /* Serve front end */
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(express.static(path.join(__dirname, '../../frontend/dist/')));
-//     app.get('*', (req, res) =>
-//         res.sendFile(
-//             path.resolve(__dirname, '../../', 'frontend', 'dist', 'index.html')
-//         )
-//     );
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/dist/frontend')));
+    app.get('*', (req, res) =>
+        res.sendFile(
+            path.resolve(__dirname, 'frontend', 'dist', 'frontend', 'index.html')
+        )
+    );
+}
 
 /* websocket server */
 const io = require('socket.io')(http, {
